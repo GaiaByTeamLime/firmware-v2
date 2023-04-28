b21142b703ec06bddf4da620e3681bf237c34e9d
SanderBaan1 <74295294+SanderBaan1@users.noreply.github.com>
2023-04-28 10:25:28 +0200

    clean code

--- main/main.c
+++ main/main.c
@@ -16,34 +16,11 @@
 #define LOG(format, ...) /* */
 #endif
 
-// void adc_init(void)
-// {
-// 	//-------------ADC1 Init---------------// factory calibrated
-//     adc_oneshot_unit_handle_t adc1_handle;
-//     adc_oneshot_unit_init_cfg_t init_config1 = {
-//         .unit_id = ADC_UNIT_1,
-//     };
-//     ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+adc_oneshot_unit_handle_t adc1_handle;
 
-//     //-------------ADC1 Config---------------//
-//     adc_oneshot_chan_cfg_t config = {
-//         .bitwidth = ADC_BITWIDTH_DEFAULT,
-//         .atten = ADC_ATTEN_DB_11,
-//     };
-//     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_2, &config));
-//     //ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_3, &config));
-
-//     // //-------------ADC1 Calibration Init---------------//
-//     // adc_cali_handle_t adc1_cali_handle = NULL;
-//     // bool do_calibration1 = example_adc_calibration_init(ADC_UNIT_1, ADC_ATTEN_DB_11, &adc1_cali_handle);
-// }
-
-void app_main(void) {
-	int index = 0;
-	int adc_raw = 0;
-
+void adc_init(void)
+{
 	//-------------ADC1 Init---------------//
-    adc_oneshot_unit_handle_t adc1_handle;
     adc_oneshot_unit_init_cfg_t init_config1 = {
         .unit_id = ADC_UNIT_1,
 		.ulp_mode = ADC_ULP_MODE_DISABLE, //sets if the ADC will be working under super low power mode (now disabled)
@@ -65,6 +42,13 @@ void app_main(void) {
 		.bitwidth = ADC_BITWIDTH_DEFAULT,
 	};
 	adc_cali_create_scheme_curve_fitting(&cali_config, &adc1_cali_handle);
+}
+
+void app_main(void) {
+	int index = 0;
+	int adc_raw = 0;
+
+	adc_init();
 
 	while (1) {
 		ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL_2, &adc_raw)); //aangeraden manier, werkt niet in ISR context (daarvoor is de functie adc_oneshot_read_isr())
@@ -73,66 +57,3 @@ void app_main(void) {
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
 }
-
-// /*---------------------------------------------------------------
-//         ADC Calibration
-// ---------------------------------------------------------------*/
-// static bool example_adc_calibration_init(adc_unit_t unit, adc_atten_t atten, adc_cali_handle_t *out_handle)
-// {
-//     adc_cali_handle_t handle = NULL;
-//     esp_err_t ret = ESP_FAIL;
-//     bool calibrated = false;
-
-// #if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
-//     if (!calibrated) {
-//         ESP_LOGI(TAG, "calibration scheme version is %s", "Curve Fitting");
-//         adc_cali_curve_fitting_config_t cali_config = {
-//             .unit_id = unit,
-//             .atten = atten,
-//             .bitwidth = ADC_BITWIDTH_DEFAULT,
-//         };
-//         ret = adc_cali_create_scheme_curve_fitting(&cali_config, &handle);
-//         if (ret == ESP_OK) {
-//             calibrated = true;
-//         }
-//     }
-// #endif
-
-// #if ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
-//     if (!calibrated) {
-//         ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
-//         adc_cali_line_fitting_config_t cali_config = {
-//             .unit_id = unit,
-//             .atten = atten,
-//             .bitwidth = ADC_BITWIDTH_DEFAULT,
-//         };
-//         ret = adc_cali_create_scheme_line_fitting(&cali_config, &handle);
-//         if (ret == ESP_OK) {
-//             calibrated = true;
-//         }
-//     }
-// #endif
-
-//     *out_handle = handle;
-//     if (ret == ESP_OK) {
-//         ESP_LOGI(TAG, "Calibration Success");
-//     } else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
-//         ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
-//     } else {
-//         ESP_LOGE(TAG, "Invalid arg or no memory");
-//     }
-
-//     return calibrated;
-// }
-
-// static void example_adc_calibration_deinit(adc_cali_handle_t handle)
-// {
-// #if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
-//     ESP_LOGI(TAG, "deregister %s calibration scheme", "Curve Fitting");
-//     ESP_ERROR_CHECK(adc_cali_delete_scheme_curve_fitting(handle));
-
-// #elif ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
-//     ESP_LOGI(TAG, "deregister %s calibration scheme", "Line Fitting");
-//     ESP_ERROR_CHECK(adc_cali_delete_scheme_line_fitting(handle));
-// #endif
-// }
