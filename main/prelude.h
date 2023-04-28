b98265b291f0a9d8905462c3a759dcf9cf165baf
jaschutte <34577095+jaschutte@users.noreply.github.com>
2023-04-28 11:45:33 +0200

    added error log & pass error defines

--- main/prelude.h
+++ main/prelude.h
@@ -4,12 +4,25 @@
 
 #include "esp_log.h"
 
+// This is kind of reduntent, as the ESP already has log levels internally
+// Though those are runtime checks, this is removed during compile time
+// Which is preferable
+// Still though
 #ifdef DEBUG
-#define LOG(...) \
-	ESP_LOG_LEVEL_LOCAL(ESP_LOG_INFO, __func__, __VA_ARGS__)
+#define LOG(...) ESP_LOG_LEVEL_LOCAL(ESP_LOG_INFO, __func__, __VA_ARGS__)
+#define ELOG(...) ESP_LOG_LEVEL_LOCAL(ESP_LOG_ERROR, __func__, __VA_ARGS__)
 #else
 #define LOG(...) /* */
+#define ELOG(...) /* */
 #endif
 
-#endif
+#define PASS_ERROR(code, error_msg) \
+	{ \
+		esp_err_t err = (code); \
+		if ((err) != ESP_OK) { \
+			ELOG("%i: " error_msg, err); \
+			return err; \
+		} \
+	}
 
+#endif
