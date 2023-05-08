6b503d10f58ae6ff338f5b33d4f7e4e904bc7b4e
jaschutte <34577095+jaschutte@users.noreply.github.com>
2023-05-08 15:43:26 +0200

    monolithic init function

--- main/wifi/wifi.c
+++ main/wifi/wifi.c
@@ -1,9 +1,61 @@
 
 #include "../prelude.h"
 
+#include <esp_event.h>
+#include <esp_netif.h>
+#include <esp_wifi.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/event_groups.h>
+#include <nvs_flash.h>
+
+#include "esp_err.h"
+#include "esp_wifi_types.h"
 #include "wifi.h"
 
-void test(void) {
-	LOG("Testing");
 }
 
+esp_err_t wifi_init(const char *ssid, const char *password) {
+	wifi_event_group = xEventGroupCreate();
+
+	PASS_ERROR(esp_netif_init(), "Failed to init Network Stack");
+	PASS_ERROR(esp_event_loop_create_default(), "Failed to create event loop");
+	esp_netif_create_default_wifi_sta();
+
+	// Setup the init config
+	wifi_init_config_t init_config = WIFI_INIT_CONFIG_DEFAULT();
+	init_config.nvs_enable = false;
+	init_config.nano_enable = false;
+	PASS_ERROR(esp_wifi_init(&init_config), "Failed to init WiFi");
+	LOG("WiFi succesfully initialised");
+	// Create a config struct
+	wifi_config_t wifi_config = {.sta = {
+									 .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+								 }};
+
+	// Copy the SSID and the password to the wifi config
+	uint8_t copy_index = 0;
+	while (*ssid) {
+		wifi_config.sta.ssid[copy_index++] = *ssid++;
+	}
+	copy_index = 0;
+	while (*password) {
+		wifi_config.sta.password[copy_index++] = *password++;
+	}
+
+	// We have to set the mode BEFORE setting the config
+	PASS_ERROR(esp_wifi_set_mode(WIFI_MODE_STA), "Unable to set WiFi mode");
+	PASS_ERROR(esp_wifi_set_config(WIFI_IF_STA, &wifi_config),
+			   "Unable to configure WiFi");
+	PASS_ERROR(
+		esp_wifi_set_ps(WIFI_PS_NONE),
+		"Unable to set Power Saving mode"); // TODO: figure out which PS we need
+	LOG("WiFi configured");
+
+	PASS_ERROR(esp_wifi_start(), "Unable to begin WiFi");
+	LOG("WiFi started");
+
+	PASS_ERROR(esp_wifi_connect(), "Unable to connect to AP");
+	LOG("WiFi connecting");
+
+	return ESP_OK;
+}
--- main/wifi/wifi.h
+++ main/wifi/wifi.h
@@ -2,7 +2,16 @@
 #ifndef __WIFI_HEADER__
 #define __WIFI_HEADER__
 
-void test(void);
+#include <esp_err.h>
+
+/* Setup the entire WiFi driver and immediately connect to the network
+ * At the current moment, we only support WPA2-PSK encryption method
+ *
+ * @param ssid The SSID of the WiFi network to connect to
+ * @param password The password of the WiFi network to connect to
+ * @return esp_err_t Any error the WiFi driver may have encountered
+*/
+esp_err_t wifi_init(const char* ssid, const char* password);
 
 #endif
 
