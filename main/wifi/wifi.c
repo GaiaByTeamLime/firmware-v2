ea6667937a176ec4d49450293f76301a9cdbe7b6
jaschutte <34577095+jaschutte@users.noreply.github.com>
2023-05-08 15:45:35 +0200

    event based reconnection

--- main/wifi/wifi.c
+++ main/wifi/wifi.c
@@ -12,6 +12,25 @@
 #include "esp_wifi_types.h"
 #include "wifi.h"
 
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAILED_BIT BIT1
+
+static EventGroupHandle_t wifi_event_group;
+
+static void ip_event_handler(void *arg, esp_event_base_t event_base,
+							 int32_t event_id, void *event_data) {
+	// We got a connection!
+	if (event_id == IP_EVENT_STA_GOT_IP) {
+		xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
+	}
+}
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+							   int32_t event_id, void *event_data) {
+	// Just keep retrying on disconnects
+	if (event_id == WIFI_EVENT_STA_DISCONNECTED) {
+		esp_wifi_connect();
+	}
 }
 
 esp_err_t wifi_init(const char *ssid, const char *password) {
@@ -27,6 +46,16 @@ esp_err_t wifi_init(const char *ssid, const char *password) {
 	init_config.nano_enable = false;
 	PASS_ERROR(esp_wifi_init(&init_config), "Failed to init WiFi");
 	LOG("WiFi succesfully initialised");
+
+	// Register events
+	esp_event_handler_instance_t event_wifi_instance, event_ip_instance;
+	esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
+										&wifi_event_handler, NULL,
+										&event_wifi_instance);
+	esp_event_handler_instance_register(IP_EVENT, ESP_EVENT_ANY_ID,
+										&ip_event_handler, NULL,
+										&event_ip_instance);
+
 	// Create a config struct
 	wifi_config_t wifi_config = {.sta = {
 									 .threshold.authmode = WIFI_AUTH_WPA2_PSK,
@@ -56,6 +85,18 @@ esp_err_t wifi_init(const char *ssid, const char *password) {
 
 	PASS_ERROR(esp_wifi_connect(), "Unable to connect to AP");
 	LOG("WiFi connecting");
+
+	EventBits_t bits = xEventGroupWaitBits(wifi_event_group,
+										   WIFI_CONNECTED_BIT | WIFI_FAILED_BIT,
+										   pdFALSE, pdFALSE, portMAX_DELAY);
+
+	if (bits & WIFI_CONNECTED_BIT) {
+		LOG("Great success! We connected!!");
+	} else if (bits & WIFI_FAILED_BIT) {
+		LOG("DEATH.");
+	} else {
+		LOG("DEATH AGAIN.");
+	}
 
 	return ESP_OK;
 }
