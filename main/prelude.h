3059dc034bbcdcc647903ec8bbcddc3abea840de
jaschutte <34577095+jaschutte@users.noreply.github.com>
2023-05-09 10:37:04 +0200

    Setup prelude

--- main/prelude.h
+++ main/prelude.h
@@ -1,8 +1,7 @@
-
 #ifndef __PRELUDE_HEADER__
 #define __PRELUDE_HEADER__
 
-#include <esp_log.h>
+#include "esp_log.h"
 
 // This is kind of reduntent, as the ESP already has log levels internally
 // Though those are runtime checks, this is removed during compile time
@@ -12,15 +11,17 @@
 #define LOG(...) ESP_LOG_LEVEL_LOCAL(ESP_LOG_INFO, __func__, __VA_ARGS__)
 #define ELOG(...) ESP_LOG_LEVEL_LOCAL(ESP_LOG_ERROR, __func__, __VA_ARGS__)
 #else
-#define LOG(...)  /* */
-#define ELOG(...) /* */
+#define LOG(...)			   /* */
+#define ELOG(...)			   /* */
 #endif
 
+// TODO: make this only convert error code to message on debug mode
 #define PASS_ERROR(code, error_msg) \
 	{ \
 		esp_err_t err = (code); \
 		if ((err) != ESP_OK) { \
-			ELOG("%i: " error_msg, err); \
+			const char *err_name = esp_err_to_name(err); \
+			ELOG("(%s) 0x%x: " error_msg, err_name, err); \
 			return err; \
 		} \
 	}
