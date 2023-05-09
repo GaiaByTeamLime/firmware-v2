02baf9b02a42ac319ee5dde48d53f4ce027d03cd
sander2324 <43219603+sander2324@users.noreply.github.com>
2023-05-09 15:07:58 +0200

    Remove Persistent Storage demo code in main

--- main/main.c
+++ main/main.c
@@ -10,20 +10,13 @@
 void app_main(void) {
 	int index = 0;
 
+	persistent_storage_init();
 	esp_err_t message = adc_init();
 	if (message != ESP_OK) {
 		while (1) {
 			LOG("ADC1 Init Error");
 		}
 	}
-
-	persistent_storage_init();
-
-	char ssid[WIFI_SSID_CHAR_BUFFER_LENGTH];
-	char passwd[WIFI_PASSWORD_CHAR_BUFFER_LENGTH];
-	persistent_storage_get_wifi(ssid, passwd);
-
-	LOG("SSID: %s\tPassword: %s", ssid, passwd);
 
 	while (1) {
 		pull_latest_data();
