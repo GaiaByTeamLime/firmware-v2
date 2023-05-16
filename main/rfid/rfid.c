26db29e4502d8faaaa6772de2cd227bfa67e4fbb
jaschutte <34577095+jaschutte@users.noreply.github.com>
2023-05-16 11:34:40 +0200

    fixed read bytes

--- main/rfid/rfid.c
+++ main/rfid/rfid.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 
-#include "spi.h"
+#include "prelude.h"
 #include "rfid.h"
+#include "spi.h"
 
 esp_err_t rfid_init(spi_device_handle_t* handle) {
 	const spi_device_interface_config_t device_config = {
@@ -19,19 +20,22 @@ esp_err_t rfid_init(spi_device_handle_t* handle) {
 	return ESP_OK;
 }
 
-esp_err_t rfid_send_register(spi_device_handle_t* handle, rfid_pcd_register_t reg, uint8_t data) {
-	uint8_t send_data[2] = { reg, data };
+esp_err_t rfid_send_register(
+	spi_device_handle_t* handle, rfid_pcd_register_t reg, uint8_t data
+) {
+	uint8_t send_data[2] = {reg, data};
 	return spi_send_bytes(handle, send_data, 2);
 }
 
-esp_err_t rfid_send_command(spi_device_handle_t* handle, rfid_pcd_command_t command) {
+esp_err_t
+rfid_send_command(spi_device_handle_t* handle, rfid_pcd_command_t command) {
 	return rfid_send_register(handle, COMMAND_REG, command);
 }
 
 esp_err_t rfid_read_registers(
-	spi_device_handle_t* handle, const rfid_pcd_register_t* registers, uint8_t* buffer, const uint16_t length
+	spi_device_handle_t* handle, const rfid_pcd_register_t* registers,
+	uint8_t* buffer, const uint16_t length
 ) {
-	uint8_t shifted_buffer[length + 1];
 	rfid_pcd_register_t read_registers[length];
 
 	// Make sure the registers we send are marked as 'read'
@@ -39,17 +43,20 @@ esp_err_t rfid_read_registers(
 		read_registers[index] = registers[index] | 0x80;
 	}
 
-	spi_transaction_t transaction = {0};
-	transaction.tx_buffer = read_registers;
-	transaction.length = (length * 2) * 8;
-	transaction.rx_buffer = shifted_buffer;
+	// Create small buffers for each transaction
+	uint8_t current_register[2] = {0}; // [0] = The register, [1] = Always 0
+	uint8_t rx_buffer[2]; // [0] = Ignore, [1] = The data
+	for (uint16_t index = 0; index < length; index++) {
+		current_register[0] = read_registers[index];
 
-	// Transmit
-	PASS_ERROR(spi_device_transmit(*handle, &transaction), "Unable to transmit");
+		spi_transaction_t transaction = {0};
+		transaction.tx_buffer = current_register;
+		transaction.length = 16;
+		transaction.rx_buffer = rx_buffer;
 
-	// Copy the shifted buffer into the correct position
-	for (uint16_t index = 1; index < length + 1; index++) {
-		buffer[index - 1] = shifted_buffer[index];
+		PASS_ERROR(spi_device_transmit(*handle, &transaction), "Unable to transmit");
+
+		buffer[index] = rx_buffer[1];
 	}
 
 	return ESP_OK;
