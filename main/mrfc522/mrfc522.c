81375edd88eafd68ecb404a6f5025dba1adc67f2
jaschutte <34577095+jaschutte@users.noreply.github.com>
2023-05-16 15:52:55 +0200

    FIFO transaction start

--- main/mrfc522/mrfc522.c
+++ main/mrfc522/mrfc522.c
@@ -8,6 +8,14 @@
 // https://github.com/miguelbalboa/rfid/blob/master/src/MFRC522.cpp
 
 esp_err_t mrfc522_fifo_transaction(spi_device_handle_t* handle) {
+	PASS_ERROR(rfid_write_register(handle, COMMAND_REG, PCD_IDLE), "");
+	PASS_ERROR(rfid_write_register(handle, COM_IRQ_REG, 0x7f), "");
+	PASS_ERROR(rfid_write_register(handle, FIFO_LEVEL_REG, 0x80), "");
+
+	const uint16_t length = 1;
+	uint8_t data[length];
+	PASS_ERROR(rfrid_write_register_datastream(handle, FIFO_DATA_REG, data, length), "");
+
 	return ESP_OK;
 }
 
@@ -26,7 +34,8 @@ esp_err_t mrfc522_init(spi_device_handle_t* handle) {
 	PASS_ERROR(rfid_write_register(handle, TX_ASK_REG, 0x40), "Unable to init MRFC522");
 	PASS_ERROR(rfid_write_register(handle, MODE_REG, 0x3d), "Unable to init MRFC522");
 
-	
+	mrfc522_fifo_transaction(handle);
+
 	return ESP_OK;
 }
 
