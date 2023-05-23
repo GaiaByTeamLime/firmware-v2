f398e274d7b07d77f8e8f958188a9d48b5db2eac
jaschutte <34577095+jaschutte@users.noreply.github.com>
2023-05-23 13:29:30 +0200

    Extracted magic numbers and put them in defines

--- main/mrfc522/mrfc522.c
+++ main/mrfc522/mrfc522.c
@@ -1,8 +1,4 @@
 
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <stdlib.h>
-
 #include "mrfc522.h"
 #include "prelude.h"
 #include "rfid.h"
@@ -11,17 +7,18 @@
 
 esp_err_t mrfc522_transceive_picc(
 	spi_device_handle_t* handle, uint8_t* write_data,
-	const uint16_t write_length, uint8_t* read_data, const uint16_t read_length
+	const uint16_t write_length, uint8_t* read_data, const uint16_t read_length,
+	const uint8_t framing_bits
 ) {
 	PASS_ERROR(
 		rfid_send_command(handle, PCD_IDLE), "Unable to stop active command"
 	);
 	PASS_ERROR(
-		rfid_write_register(handle, COM_IRQ_REG, 0x7f),
+		rfid_write_register(handle, COM_IRQ_REG, CLEAR_IRQS),
 		"Unable to clear all IRq's"
 	);
 	PASS_ERROR(
-		rfid_write_register(handle, FIFO_LEVEL_REG, 0x80),
+		rfid_write_register(handle, FIFO_LEVEL_REG, FLUSH_FIFO),
 		"Unable to flush FIFO"
 	);
 	PASS_ERROR(
@@ -31,7 +28,7 @@ esp_err_t mrfc522_transceive_picc(
 		"Unable to stream data to FIFO buffer"
 	);
 	PASS_ERROR(
-		rfid_write_register(handle, BIT_FRAMING_REG, 0),
+		rfid_write_register(handle, BIT_FRAMING_REG, framing_bits),
 		"Unable to set bit adjustment"
 	);
 	PASS_ERROR(
@@ -39,13 +36,13 @@ esp_err_t mrfc522_transceive_picc(
 		"Unable to signal start of transmission"
 	);
 	PASS_ERROR(
-		rfid_write_register(handle, BIT_FRAMING_REG, 0x80),
+		rfid_write_register(handle, BIT_FRAMING_REG, 0x80 | framing_bits),
 		"Unable to signal start of transmission"
 	)
 
 	rfid_pcd_register_t registers[] = {COM_IRQ_REG, ERROR_REG, FIFO_LEVEL_REG};
 
-	uint16_t attempts_left = 500; // Wait max 35ms for transmission
+	uint16_t attempts_left = 5000;
 	uint8_t output_buffer[1] = {0};
 	while (attempts_left) {
 		PASS_ERROR(
@@ -53,12 +50,13 @@ esp_err_t mrfc522_transceive_picc(
 			"Unable to read register"
 		);
 		// If the IRq fired, don't wait needlessly, break out
-		// LOG("=> %02x", output_buffer[0]);
-		if (output_buffer[0] & 0x30) {
+		if (output_buffer[0] & DATASTREAM_STOP_IRQ) {
 			break;
 		}
-		// Wait before retrying
-		vTaskDelay(pdMS_TO_TICKS(1));
+		if (output_buffer[0] & TIMEOUT_IRQ) {
+			LOG("Timer Timeout");
+			return ESP_ERR_TIMEOUT;
+		}
 		attempts_left--;
 	}
 	if (!attempts_left) {
@@ -68,14 +66,15 @@ esp_err_t mrfc522_transceive_picc(
 
 	// Make sure no weird error happened in the meantime
 	rfid_read_registers(handle, registers + 1, output_buffer, 1);
-	if (output_buffer[0] & 0x13) {
+	if (output_buffer[0] & INVALID_STATE) {
 		ELOG("Error during transceive!");
 		return ESP_ERR_INVALID_STATE;
 	}
 
-	// Read back the result!
+	// Rea back the result!
 	rfid_read_registers(handle, registers + 2, output_buffer, 1);
 	uint8_t bytes_in_fifo = output_buffer[0];
+	// LOG("Bytes in FIFO: %d", bytes_in_fifo);
 	rfid_read_register_datastream(
 		handle, FIFO_DATA_REG, read_data, bytes_in_fifo
 	);
@@ -96,10 +95,10 @@ esp_err_t mrfc522_calculate_crc(
 		rfid_send_command(handle, PCD_IDLE), "Unable to stop active command"
 	);
 	PASS_ERROR(
-		rfid_write_register(handle, DIV_IRQ_REG, 0x04), "Unable to clear CRCIRq"
+		rfid_write_register(handle, DIV_IRQ_REG, CLEAR_CRC_IRQ), "Unable to clear CRCIRq"
 	);
 	PASS_ERROR(
-		rfid_write_register(handle, FIFO_LEVEL_REG, 0x80),
+		rfid_write_register(handle, FIFO_LEVEL_REG, FLUSH_FIFO),
 		"Unable to flush FIFO buffer"
 	);
 	PASS_ERROR(
@@ -113,7 +112,7 @@ esp_err_t mrfc522_calculate_crc(
 	);
 
 	// Wait for the IRQ signal to fire
-	uint16_t attempts_left = 500; // Wait at max 50ms
+	uint16_t attempts_left = 5000;
 	uint8_t irq_buffer[1] = {0};
 	while (attempts_left) {
 		PASS_ERROR(
@@ -125,7 +124,6 @@ esp_err_t mrfc522_calculate_crc(
 			break;
 		}
 		// Wait before retrying
-		vTaskDelay(pdMS_TO_TICKS(1));
 		attempts_left--;
 	}
 	// Check if we didn't time out
@@ -159,7 +157,12 @@ esp_err_t mrfc522_read_mifare(
 		"Unable to calculate CRC"
 	);
 	// Send the transceive request
-	return mrfc522_transceive_picc(handle, buffer, 4, buffer, buffer_size);
+	const uint8_t framing = 0x00;
+	return mrfc522_transceive_picc(
+		handle, buffer, 4, buffer, buffer_size, framing
+	);
+}
+
 esp_err_t mrfc522_wakeup(spi_device_handle_t* handle) {
 	uint8_t command[1] = {MIFARE_REQA};
 	uint8_t buffer[2];
@@ -226,44 +229,55 @@ esp_err_t mrfc522_init(spi_device_handle_t* handle) {
 	PASS_ERROR(
 		rfid_write_register(handle, RX_MODE_REG, 0x00), "Unable to init MRFC522"
 	);
-	PASS_ERROR(
-		rfid_write_register(handle, MOD_WIDTH_REG, 0x00),
-		"Unable to init MRFC522"
-	);
 
 	// Configure timer
 	PASS_ERROR(
-		rfid_write_register(handle, T_MODE_REG, 0x80), "Unable to init MRFC522"
+		rfid_write_register(handle, T_MODE_REG, TIMER_MODE), "Unable to init MRFC522"
 	);
 	PASS_ERROR(
-		rfid_write_register(handle, T_PRESCALER_REG, 0xa9),
+		rfid_write_register(handle, T_PRESCALER_REG, TIMER_PRESCALAR),
 		"Unable to init MRFC522"
 	);
 	PASS_ERROR(
-		rfid_write_register(handle, T_RELOAD_REG_H, 0x03),
+		rfid_write_register(handle, T_RELOAD_REG_H, TIMER_DURATION >> 8),
 		"Unable to init MRFC522"
 	);
 	PASS_ERROR(
-		rfid_write_register(handle, T_RELOAD_REG_L, 0xe8),
+		rfid_write_register(handle, T_RELOAD_REG_L, (uint8_t)TIMER_DURATION),
 		"Unable to init MRFC522"
 	);
 
 	PASS_ERROR(
-		rfid_write_register(handle, TX_ASK_REG, 0x40), "Unable to init MRFC522"
+		rfid_write_register(handle, TX_ASK_REG, FORCE_100_MODULATION), "Unable to init MRFC522"
 	);
 	PASS_ERROR(
-		rfid_write_register(handle, MODE_REG, 0x3d), "Unable to init MRFC522"
+		rfid_write_register(handle, MODE_REG, CRC_PRESET_MODE), "Unable to init MRFC522"
 	);
 
 	// Enable the antenna
 	mrfc522_enable_antenna(handle);
 
-	uint8_t buffer[64] = {0};
-	mrfc522_read_mifare(handle, 4, buffer, 64);
+	// Wake up the rfid reader
+	mrfc522_wakeup(handle);
 
+	// Read actual data from the tag
+	uint8_t buffer[18] = {0};
+	mrfc522_read_mifare(handle, 4, buffer, 18);
+
+	// Output the data
 	LOG("SUCCES:");
-	for (uint8_t i = 0; i < 64; i++) {
-		LOG("\t%x", buffer[i]);
+	for (uint8_t i = 0; i < 18; i++) {
+		LOG("\t%x (%c)", buffer[i], buffer[i]);
+	}
+	LOG("END");
+
+	// Read actual data from the tag
+	mrfc522_read_mifare(handle, 4 + 4, buffer, 18);
+
+	// Output the data
+	LOG("SUCCES:");
+	for (uint8_t i = 0; i < 18; i++) {
+		LOG("\t%x (%c)", buffer[i], buffer[i]);
 	}
 	LOG("END");
 
