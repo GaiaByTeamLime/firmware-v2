48799ed8c519bf3d7d03ce31be7b89c4a092ddc2
SanderBaan1 <74295294+SanderBaan1@users.noreply.github.com>
2023-05-30 12:24:24 +0200

    removed duplicate enum and update documentation

--- docs/api_reference/adc.rst
+++ docs/api_reference/adc.rst
@@ -4,4 +4,5 @@ ADC
 The ADC module is used to read analog values from the ADC pins. The ADC pins are connected to sensors.
 
 .. doxygenfile:: adc.h
-    
+
+
--- main/adc/adc.h
+++ main/adc/adc.h
@@ -4,21 +4,33 @@
 #include <esp_err.h>
 
 /**
- * Number of ADC ports
+ * The amount of ADC ports used
+ * 
+ * @warning If you add or remove an item from the `adc1_port_t` enum, you need to change this define
  *
  */
 #define ADC1_PORTS_AMOUNT 1
 
 /**
- * Enum to select the adc port
- *
+ * Enum to select the corresponding adc port
+ * 
+ * @warning If you add or remove an item from this enum, you need to change the ADC1_PORTS_AMOUNT 
+ * with the amount of items in this enum. Also, you need to add or remove the corresponding port from
+ * the `adc1_ports` array in adc.c. The 0 index of the array is the 0 index of the enum
+ * 
  */
 typedef enum {
-	ADC1_LDR = 0,
+	ADC1_LDR, /*!< Is connected to PORT 3 from the esp32, because it is a GPIO only port */
 } adc1_port_t;
 
 /**
  * Struct to store the adc data
+ * 
+ * @note The data is stored in the `adc_result` array in adc.c. The 0 index of the array is 
+ * the 0 index of the enum `adc_result`
+ * 
+ * @param messageResult Result of the operation
+ * @param data The data from the ADC
  *
  */
 typedef struct {
@@ -27,34 +39,25 @@ typedef struct {
 } adc_data_t;
 
 /**
- * enum to select the adc port
- * 
- * @warning if you add some items, you need to change the ADC1_PORTS_AMOUNT and adc1_ports arrays in adc.c
- * 
- */
-typedef enum {
-	ADC1_LDR, //Port 3
-} adc1_port_t;
-
-/**
  * Initialize ADC1
  *
- * @return esp_err_t
+ * @return Result of the initialization
  */
 esp_err_t adc_init(void);
 
 /**
- * Get the Data from the ADC
- * data is stored in adc_data
+ * Update the Data from the ADC
+ * 
+ * @note Data is stored in adc_data
  */
 void pull_latest_data(void);
 
 /**
- * Get the adc data object
+ * Get the adc data from the specified port
  *
- * @param port (which sensordata to get)
- * @param data (pointer to the data)
- * @return esp_err_t
+ * @param port Which port to get the data from
+ * @param data Pointer in wich the data will be stored
+ * @return Result of the data collection
  */
 esp_err_t get_adc_data(adc1_port_t port, uint32_t* data);
 
