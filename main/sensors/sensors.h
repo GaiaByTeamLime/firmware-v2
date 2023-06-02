02723a24e1f7fa380c743f13357072d08594d21a
AE <nielserikvandam@gmail.com>
2023-06-02 12:27:28 +0200

    Temp update to check some ADC stuff on another branch.

--- main/sensors/sensors.h
+++ main/sensors/sensors.h
@@ -3,13 +3,13 @@
 
 #include "../prelude.h"
 
-#include <esp_err.h>
 #include <driver/gpio.h>
 #include <driver/gptimer.h>
+#include <esp_err.h>
 
 #define CAPACITY_SENSOR_PIN GPIO_NUM_1
+#define BATTERY_MEASUREMENT_PIN GPIO_NUM_2
 #define CAPACITY_SENSOR_GPTIMER_RESOLUTION_HZ 40000000
-
 
 esp_err_t sensors_init();
 esp_err_t measure_soil_capacity();
