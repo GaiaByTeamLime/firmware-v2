4af5595addc8a9715a734bba674bbb9d8352238b
AE <NielsGoBrrr@email.com>
2023-06-06 14:44:42 +0200

    Formatting for Sensors.h
    Formatted the code.

--- main/sensors/sensors.h
+++ main/sensors/sensors.h
@@ -13,22 +13,24 @@
 
 /**
  * Initialize all sensors.
- * 
+ *
  * @return Potential errors.
-*/
+ */
 esp_err_t sensors_init();
 
 /**
- * Function to take measurements, and place the taken measurements into a array that is given in the form of a pointer.
- * During debugging mode, 3 log statements will display the first 3 sensor measurements stored in the table given.
- * 
+ * Function to take measurements, and place the taken measurements into a array
+ * that is given in the form of a pointer. During debugging mode, 3 log
+ * statements will display the first 3 sensor measurements stored in the table
+ * given.
+ *
  * @param data A pointer to a uint32_t array that will contain the sensor data.
- * 
- * @warning Make sure that the given array is the same size as amount of sensors. At the time of writing this,
- * this number is 3.
+ *
+ * @warning Make sure that the given array is the same size as amount of
+ * sensors. At the time of writing this, this number is 3.
  *
  * @return Potential errors.
-*/
+ */
 esp_err_t measure_sensors(uint32_t* data);
 
 #endif
